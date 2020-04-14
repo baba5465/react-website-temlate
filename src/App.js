bfba88920c45077438b891cad004c48276f62cdd
baba5465 <rishikesh.tiwari5465@gmail.com>
2020-04-14 13:51:25 +0530

    Simple website template using react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import NavBar from './Navbar';
+import Footer from './FooterSection';
+import Hero from './HeroSection';
+import AppSection from './AppSection';
+import CardSection from './CardSection';
 
 const App = () => (
   <div>
     <NavBar />
+    <Hero />
+    <AppSection />
+    <CardSection />
+    <Footer />
   </div>
 );
 
